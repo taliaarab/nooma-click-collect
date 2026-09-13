df4cdbe44bb0f05ce0d585888902db7840894be8
taliaarab <talia.arab98@gmail.com>
2026-09-13 15:15:34 +0200

    Ajuster automatiquement la hauteur Click & Collect

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,30 @@
+import { useEffect } from 'react';
 import { Shop } from './components/Shop';
 
 export default function App() {
+  useEffect(() => {
+    const sendHeight = () => {
+      window.parent.postMessage(
+        {
+          type: 'nooma:resize',
+          height: document.documentElement.scrollHeight,
+        },
+        '*'
+      );
+    };
+
+    const observer = new ResizeObserver(sendHeight);
+    observer.observe(document.documentElement);
+
+    window.addEventListener('load', sendHeight);
+    sendHeight();
+
+    return () => {
+      observer.disconnect();
+      window.removeEventListener('load', sendHeight);
+    };
+  }, []);
+
   return (
     <div className="min-h-screen bg-background">
       <Shop />
